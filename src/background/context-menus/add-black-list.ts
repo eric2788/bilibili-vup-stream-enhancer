6f6214a6547adde718fc38b3b2926a83dbdc7b2a
eric2788 <tsukiko852@gmail.com>
2023-12-05 22:23:51 +0800

    Add new files and dependencies

---
+++ src/background/context-menus/add-black-list.ts
@@ -0,0 +1,35 @@
+
+import storage from '~/utils/storage'
+import { sendNotify } from '~utils/messaging'
+import { sendViaRelay } from '@plasmohq/messaging/relay'
+import { getRoomId } from '~utils/misc'
+
+export const properties: chrome.contextMenus.CreateProperties = {
+    id: 'add-black-list',
+    title: '添加到黑名单',
+    documentUrlPatterns: ['https://live.bilibili.com/*'],
+    contexts: ['page'],
+    enabled: true
+}
+
+
+export default async function (info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab): Promise<void> {
+    const settings = await storage.get<any>('settings')
+    const url = new URL(info.pageUrl)
+    const roomId = getRoomId(url.pathname)
+    if (!roomId) {
+        console.warn(`unknown room id (${url.pathname})`)
+        return;
+    }
+    if (!settings.blacklistRooms.includes(roomId)) {
+        await sendViaRelay({
+            name: 'black-list',
+            body: { roomId }
+        })
+    } else {
+        await sendNotify({
+            title: '你已添加过此房间到黑名单。',
+            message: '已略过操作。'
+        })
+    }
+}
---
+++ src/utils/misc.ts
@@ -0,0 +1,20 @@
+export async function sleep(ms: number) {
+    return new Promise((res,) => setTimeout(res, ms))
+}
+
+export function toTimer(secs: number): string {
+    const hr = Math.floor(secs / 3600);
+    secs %= 3600;
+    const min = Math.floor(secs / 60);
+    secs %= 60;
+
+    const mu = min > 9 ? `${min}` : `0${min}`;
+    const ms = secs > 9 ? `${secs}` : `0${secs}`;
+
+    return `${hr}:${mu}:${ms}`;
+}
+
+
+export function getRoomId(url: string = location.pathname) {
+    return parseInt(/^\/(blanc\/)?(?<id>\d+)/g.exec(url)?.groups?.id)
+}
