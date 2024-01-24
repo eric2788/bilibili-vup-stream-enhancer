d3de06b60f314a11233ea629ee9f6a71e37b4a90
eric2788 <tsukiko852@gmail.com>
2024-01-24 14:22:39 +0800

    updated playwright config and make upload report always

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'github' : 'list',
+  reporter: process.env.CI ? [
+    ['github'],
+    ['list', { printSteps: true }],
+    ['html', { open: 'never' }]
+  ] : [['list', { printSteps: true }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
