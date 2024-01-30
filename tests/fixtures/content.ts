f476ce8979b1b42199a78475d6df3a1862448920
eric2788 <tsukiko852@gmail.com>
2024-01-30 21:45:58 +0800

    changed to use cacher: theme room will be cached for all tests that needed

--- tests/fixtures/content.ts
+++ tests/fixtures/content.ts
@@ -4,6 +4,7 @@ import { extensionBase } from "./base";
 import type { PageFrame } from "@tests/helpers/page-frame";
 import logger from "@tests/helpers/logger";
 import type { LiveRoomInfo } from "@tests/utils/bilibili";
+import RoomTypeCacher from "@tests/helpers/fixed-room";
 
 export type ContentOptions = {
     isThemeRoom: boolean
@@ -17,11 +18,23 @@ export type ContentFixtures = {
     content: PageFrame // page for live room (can be iframe)
 }
 
+export type ContentWorkerFixtures = {
+    cacher: RoomTypeCacher
+}
 
-export const test = extensionBase.extend<ContentFixtures & ContentOptions>({
+export const test = extensionBase.extend<ContentFixtures & ContentOptions, ContentWorkerFixtures>({
 
     isThemeRoom: [ false, { option: true }],
-    maxRoomRetries: [30, { option: true }],
+    maxRoomRetries: [50, { option: true }],
+
+    cacher: [
+        async ({ browser }, use) => {
+            const cacher = new RoomTypeCacher(browser)
+            cacher.registerRoomType('theme', page => page.isThemePage())
+            await use(cacher)
+        },
+        { scope: 'worker' }
+    ],
 
     content: async ({ room, page }, use) => {
         page.frames().map(f => logger.debug(f.url()))
@@ -32,16 +45,12 @@ export const test = extensionBase.extend<ContentFixtures & ContentOptions>({
     },
 
     room: [
-        async ({ page, isThemeRoom, rooms, maxRoomRetries }, use) => {
+        async ({ page, isThemeRoom, rooms, maxRoomRetries, cacher }, use) => {
             const iterator = Strategy.random(rooms, maxRoomRetries || rooms.length)
-            let next = iterator.next()
-            using bilibiliPage = new BilibiliPage(page, next.value)
-            while (isThemeRoom !== (await checkThemePage(bilibiliPage, next.value))) {
-                logger.info(`房間 ${bilibiliPage.roomid} ${isThemeRoom ? '是' : '不是'}大海報的房間, 正在尋找下一房間...`)
-                next = iterator.next()
-                test.skip(next.done, `找不到${isThemeRoom ? '' : '不是'}大海報的房間。`)
-                await page.waitForTimeout(500)
-            }
+            const room = await cacher.findRoomType(isThemeRoom ? 'theme' : 'normal', iterator, isThemeRoom) // only theme room will cache
+            test.skip(room === null, `找不到${isThemeRoom ? '' : '不是'}大海報的房間。`)
+            using bilibiliPage = new BilibiliPage(page, room)
+            await bilibiliPage.enterToRoom()
             await use(bilibiliPage)
         },
         { auto: true, timeout: 0 }
@@ -49,7 +58,7 @@ export const test = extensionBase.extend<ContentFixtures & ContentOptions>({
 
     // force to theme room
     themeRoom: [
-        async ({ page, room, rooms, maxRoomRetries }, use) => {
+        async ({ page, room, rooms, maxRoomRetries, cacher }, use) => {
             // already theme page
             if (await room.isThemePage()) {
                 await use(room)
@@ -58,13 +67,9 @@ export const test = extensionBase.extend<ContentFixtures & ContentOptions>({
             // go to blank page first then go to theme page
             await page.goto('about:blank')
             const iterator = Strategy.random(rooms, maxRoomRetries || rooms.length)
-            let next = iterator.next()
-            while (!await checkThemePage(room, next.value)) {
-                logger.info(`房間 ${room.roomid} 不是大海報的房間, 正在尋找下一房間...`)
-                next = iterator.next()
-                test.skip(next.done, '找不到大海報的房間。')
-                await page.waitForTimeout(500)
-            }
+            const info = await cacher.findRoomType('theme', iterator, true)
+            test.skip(info === null, `找不到大海報的房間。`)
+            await room.enterToRoom(info)
             await use(room)
         },
         { timeout: 0}
@@ -73,8 +78,3 @@ export const test = extensionBase.extend<ContentFixtures & ContentOptions>({
 })
 
 export const expect = test.expect
-
-async function checkThemePage(page: BilibiliPage, info: LiveRoomInfo): Promise<boolean> {
-    await page.enterToRoom(info)
-    return await page.isThemePage()
-}
