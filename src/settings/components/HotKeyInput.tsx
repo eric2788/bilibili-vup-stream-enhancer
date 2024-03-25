ca1fd906ff877e79fce3020e6793d6d96663840d
eric2788 <tsukiko852@gmail.com>
2024-03-25 23:04:20 +0800

    finished settings fragment and fixed layout problem of feature settings

--- src/settings/components/HotKeyInput.tsx
+++ src/settings/components/HotKeyInput.tsx
@@ -25,6 +25,7 @@ export function HotKeyInput(props: HotKeyInputProps): JSX.Element {
         const listener = (e: KeyboardEvent) => {
             e.preventDefault()
             e.stopPropagation()
+            if (['Escape', 'Control', 'Shift'].includes(e.key)) return
             onChange?.(e)
             window.removeEventListener('keydown', listener)
             setListening(false)
@@ -33,23 +34,26 @@ export function HotKeyInput(props: HotKeyInputProps): JSX.Element {
         setListening(true)
     }
 
+    const keyDisplay = (value.ctrlKey ? 'Ctrl+' : '') + (value.shiftKey ? 'Shift+' : '') + value.key.toUpperCase()
+
     return (
         <div className="w-full flex justify-center">
             <div className="relative flex w-full">
                 <Input
+                    onClick={onListenKeyInput}
                     variant="static"
                     crossOrigin={'annoymous'}
                     type="text"
                     readOnly
                     required={!optional}
-                    value={listening ? '监听输入中...' : value.key}
-                    className="pr-20 font-mono tracking-[0.2rem] font-medium disabled:bg-transparent disabled:opacity-50 disabled:cursor-not-allowed"
+                    value={listening ? '监听输入中...' : keyDisplay}
+                    className="pr-20 font-mono font-medium disabled:bg-transparent disabled:opacity-50 disabled:cursor-not-allowed"
                     containerProps={{
                         className: "min-w-0",
                     }}
                     {...attrs}
                 />
-                <IconButton onClick={onListenKeyInput} disabled={listening}>
+                <IconButton onClick={onListenKeyInput} disabled={listening} variant="text" className="!absolute right-0 bottom-0 h-8 rounded bg-transparent disabled:opacity-50 disabled:cursor-not-allowed">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 disabled:animate-ping disabled:stroke-red-500">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 3.75H6A2.25 2.25 0 0 0 3.75 6v1.5M16.5 3.75H18A2.25 2.25 0 0 1 20.25 6v1.5m0 9V18A2.25 2.25 0 0 1 18 20.25h-1.5m-9 0H6A2.25 2.25 0 0 1 3.75 18v-1.5M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
