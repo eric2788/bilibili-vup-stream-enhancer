ce6568385b337b1a60f4ca3ddcf3bd6b5d9266a8
eric2788 <tsukiko852@gmail.com>
2024-10-22 16:23:26 +0800

    optimized open-tab singleton mechanism

--- src/background/messages/open-tab.ts
+++ src/background/messages/open-tab.ts
@@ -5,24 +5,26 @@ export type RequestBody = {
     tab?: string
     active?: boolean
     params?: Record<string, string>
-    singleton?: boolean
+    singleton?: boolean | string[]
 }
 
 const handler: PlasmoMessaging.MessageHandler<RequestBody, chrome.tabs.Tab> = async (req, res) => {
     const { url, tab, active } = req.body
     const queryString = req.body.params ? `?${new URLSearchParams(req.body.params).toString()}` : ''
     const fullUrl = tab ? chrome.runtime.getURL(`/tabs/${tab}.html${queryString}`) : url + queryString
+    const pathUrl = (tab ? chrome.runtime.getURL(`/tabs/${tab}.html`) : url) + '*'
     if (req.body.singleton) {
-        const tabs = await chrome.tabs.query({ url: fullUrl })
-        if (tabs.length) {
-            const tab = tabs[0]
-            await chrome.tabs.update(tab.id, { active: true })
-            res.send(tab)
+        const tabs = await chrome.tabs.query({ url: typeof req.body.singleton === 'boolean' ? fullUrl : pathUrl })
+        const tab = tabs.find(tab =>
+            typeof req.body.singleton === 'boolean' ||
+            req.body.singleton.some(param => new URL(tab.url).searchParams.get(param) === req.body.params[param])
+        )
+        if (tab) {
+            res.send(await chrome.tabs.update(tab.id, { active: true }))
             return
         }
     }
-    const result = await chrome.tabs.create({ url: fullUrl, active })
-    res.send(result)
+    res.send(await chrome.tabs.create({ url: fullUrl, active }))
 }
 
 
